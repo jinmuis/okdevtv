e24398e3752fed12bda5fda25d74db2e41167111
jinmu <jinmudev@gmail.com>
2017-09-14 21:30:56 +0900

    interface

---
+++ src/main/java/com/okdevtv/java/App.java
@@ -0,0 +1,21 @@
+package com.okdevtv.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App implements AppInterface {
+	private static String msg = "Hello World!";
+
+	public static void main(String[] args) {
+		print(msg);
+	}
+
+	private static void print(String x) {
+		System.out.println(x);
+	}
+
+	public String getMessage() {
+		return msg;
+	}
+}
